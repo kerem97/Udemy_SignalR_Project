9203666afc5fea3d7c2fa0d89ec48a3ada50ab06
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-11-10 00:38:17 +0300

    DtoLayer finished

---
+++ SignalR.DtoLayer/ContactDtos/ResultContactDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.ContactDtos
+{
+    public class ResultContactDto
+    {
+        public int ContactID { get; set; }
+
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
+    }
+}
