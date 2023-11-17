fa42be39466ef2d8458d8d6617756cd09ce92f58
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-11-17 23:09:47 +0300

    Default UI

---
+++ SignalR.Web.UI/ViewComponents/UILayoutComponents/_UILayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.Web.UI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalR.Web.UI/ViewComponents/UILayoutComponents/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.Web.UI.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
