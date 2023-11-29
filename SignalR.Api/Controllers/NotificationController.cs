3dc08b560787bcc8f95b3270ce224dff2e9a2008
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2023-11-29 12:20:20 +0300

    Notification APi & Consume

---
+++ SignalR.Api/Controllers/NotificationController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDtos;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet]
+
+        public IActionResult NotificationList()
+        {
+            var values = _notificationService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpGet("NotificationCountByStatusFalse")]
+        public IActionResult NotificationCountByStatusFalse()
+        {
+            return Ok(_notificationService.TNotificationCountByStatusFalse());
+        }
+        [HttpGet("GetAllNotificationByFalse")]
+        public IActionResult GetAllNotificationByFalse()
+        {
+            return Ok(_notificationService.TGetAllNotificationByFalse());
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+                Description = createNotificationDto.Description,
+                Icon = createNotificationDto.Icon,
+                Status = false,
+                Type = createNotificationDto.Type
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Ekleme İşlemi Başarıyla Gerçekleştirildi");
+        }
+
+        [HttpDelete]
+
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            _notificationService.TDelete(value);
+            return Ok("Silme İşlemi Başarıyla Gerçekleştirildi");
+
+
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Date = updateNotificationDto.Date,
+                Description = updateNotificationDto.Description,
+                Icon = updateNotificationDto.Icon,
+                Status = updateNotificationDto.Status,
+                Type = updateNotificationDto.Type
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Güncelleme İşlemi Başarıyla Gerçekleştirildi");
+        }
+        [HttpGet("NotificationStatusChangeToFalse/{id}")]
+        public IActionResult NotificationStatusChangeToFalse(int id)
+        {
+            _notificationService.TNotificationStatusChangeToFalse(id);
+            return Ok("Güncelleme yapıldı");
+        }
+        [HttpGet("NotificationStatusChangeToTrue/{id}")]
+        public IActionResult NotificationStatusChangeToTrue(int id)
+        {
+            _notificationService.TNotificationStatusChangeToTrue(id);
+            return Ok("Güncelleme yapıldı");
+        }
+
+
+    }
+}
---
+++ SignalR.Api/Mapping/NotificationMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.NotificationDtos;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.Api.Mapping
+{
+    public class NotificationMapping : Profile
+    {
+        public NotificationMapping()
+        {
+            CreateMap<Notification, ResultNotificationDto>().ReverseMap();
+            CreateMap<Notification, CreateNotificationDto>().ReverseMap();
+            CreateMap<Notification, UpdateNotificationDto>().ReverseMap();
+        }
+    }
+}
